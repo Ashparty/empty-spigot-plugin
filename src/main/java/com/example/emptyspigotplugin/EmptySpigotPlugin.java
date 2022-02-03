4ee5aae760c08df7da371f61bdbc55eb882d7372
Ashparty <75103275+Ashparty@users.noreply.github.com>
2022-02-02 21:16:29 -0700

    e

--- src/main/java/com/example/emptyspigotplugin/EmptySpigotPlugin.java
+++ src/main/java/com/example/emptyspigotplugin/EmptySpigotPlugin.java
@@ -5,12 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class EmptySpigotPlugin extends JavaPlugin {
 
   public void onEnable() {
-    System.out.println("Enabled EmptySpigotPlugin");
+    System.out.println("Enabled Cool Plugin Go BRRRR");
   }
 
   @Override
   public void onDisable() {
-    System.out.println("Disabled EmptySpigotPlugin");
+    System.out.println("Disabled cool plugin ):");
   }
 
 }
